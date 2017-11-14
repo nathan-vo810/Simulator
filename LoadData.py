fd05c9a64301ba7e62b09f16fb5c20fc0bd4d4a3
amidadragon <cs2014_hoang.ng@student.vgu.edu.vn>
2017-11-14 15:56:24 +0700

    Small fix

--- LoadData.py
+++ LoadData.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(0,144,1)
-y = np.zeros((144,17))
-list2 = [1, 2, 3, 4, 5, 6, 7 ]
-print(list2)
 data2 = []
 # with open('2012-06-01.csv', newline='') as csvfile:
 #     dataReader = csv.reader(csvfile)
@@ -22,7 +19,6 @@ with open('TotalConsumption.csv', newline='') as csvfile:
     next(dataReader)
     for data in dataReader:
         data2.append([float(i) for i in data])
-        np.append(y,data2)
 
 occupancy = [row[0] for row in data2]
 total_demand = [row[1] for row in data2]
